72e55451910e9ac300ed6a11d54845ca64a9f0cc
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-22 17:34:12 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Theodore Utomo on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy
+//
+//  Created by Theodore Utomo on 9/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
